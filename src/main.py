d9e27a44c1639f117a7a9edabf1db45015ac1e05
almaz <almazgimaev@gmail.com>
2024-02-27 23:03:34 +0100

    set dataset name

--- src/main.py
+++ src/main.py
@@ -10,6 +10,8 @@ if sly.is_development():
     load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 
+app_data = sly.app.get_data_dir()
+sly.fs.clean_dir(app_data)
 api = sly.Api()
 
 
@@ -18,6 +20,7 @@ team_id = sly.env.team_id()
 project_id = sly.env.project_id()
 dataset_id = sly.env.dataset_id(raise_not_found=False)
 task_id = sly.env.task_id()
+dataset_name = os.environ.get("datasetName", "ds0")
 
 src_dir = sly.env.folder()
 
@@ -27,7 +30,7 @@ project = api.project.get_info_by_id(project_id)
 if dataset_id:
     dataset = api.dataset.get_info_by_id(dataset_id)
 else:
-    dataset = api.dataset.create(project.id, "ds0", change_name_if_conflict=True)
+    dataset = api.dataset.create(project.id, dataset_name, change_name_if_conflict=True)
 
 project_meta = sly.ProjectMeta.from_json(api.project.get_meta(project_id))
 project_modality = project.type
@@ -41,6 +44,10 @@ importer.upload_dataset(dataset.id)
 
 # * 4. Set output project
 api.task.set_output_project(task_id, project.id)
+
+# * 5. Clean app_data directory
+sly.fs.clean_dir(app_data)
+
 
 # TODO list:
 # - [ ] self._annotations – check if it is necessary (or remove it)
