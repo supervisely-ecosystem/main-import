84c075dd10c66257418cf12face6de18a0e3ffd3
almaz <almazgimaev@gmail.com>
2024-02-27 15:51:59 +0100

    simplify code

--- src/main.py
+++ src/main.py
@@ -13,36 +13,22 @@ api = sly.Api()
 
 
 workspace_id = sly.env.workspace_id()
-project_id = sly.env.project_id(raise_not_found=False)
-dataset_id = sly.env.dataset_id(raise_not_found=False)
-project_modality = os.getenv("PROJECT_MODALITY")
-src_dir = os.getenv("SRC_DIR")
+team_id = sly.env.team_id()
+project_id = sly.env.project_id()
+dataset_id = sly.env.dataset_id()
 
-if not src_dir:
-    raise RuntimeError("SRC_DIR is not specified.")
-if not project_modality:
-    raise RuntimeError("PROJECT_MODALITY is not specified.")
-elif project_modality not in sly.ProjectType:
-    raise RuntimeError("PROJECT_MODALITY is not valid.")
+src_dir = sly.env.folder()
 
 
 # * 1. Get project and dataset infos
-
+project = api.project.get_info_by_id(project_id)
 if dataset_id:
     dataset = api.dataset.get_info_by_id(dataset_id)
-    project = api.project.get_info_by_id(dataset.project_id)
 else:
-    if project_id:
-        project = api.project.get_info_by_id(project_id)
-    else:
-        project = api.project.create(
-            workspace_id,
-            "Imported Project",
-            change_name_if_conflict=True,
-            type=project_modality,
-        )
     dataset = api.dataset.create(project.id, "ds0", change_name_if_conflict=True)
+
 project_meta = sly.ProjectMeta.from_json(api.project.get_meta(project_id))
+project_modality = project.type
 
 
 # * 2. initialize importer
