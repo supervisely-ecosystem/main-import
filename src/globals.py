8ac158e28b9f53d8fb1ddcb9ff2e2cb5ac951617
almaz <almazgimaev@gmail.com>
2024-03-20 09:17:35 +0100

    wip

--- src/globals.py
+++ src/globals.py
@@ -1,12 +1,14 @@
 import os
 
-import supervisely as sly
 from dotenv import load_dotenv
+
+import supervisely as sly
 
 if sly.is_development():
     load_dotenv("local.env")
     load_dotenv(os.path.expanduser("~/supervisely.env"))
 
+api = sly.Api()
 
 app_data = sly.app.get_data_dir()
 sly.fs.clean_dir(app_data)
