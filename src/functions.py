3825855fb6ea5d4a57f90a9e0e48cc2e49bb146e
almaz <almazgimaev@gmail.com>
2024-04-18 13:50:22 +0200

    update debug info

--- src/functions.py
+++ src/functions.py
@@ -13,21 +13,25 @@ def handle_exception_and_stop(exc: Exception, msg: str = "Error"):
         handle_exception as sly_handle_exception,
     )
 
+    debug_info = {
+        "team_id": g.team_id,
+        "workspace_id": g.workspace_id,
+        "project_id": g.project_id,
+        "dataset_id": g.dataset_id,
+        "project_modality": g.project_modality,
+        "src_dir": g.input_path,
+    }
+
     handled_exc = sly_handle_exception(exc)
     if handled_exc is not None:
         g.api.task.set_output_error(g.task_id, handled_exc.title, handled_exc.message)
-        handled_exc.log_error_for_agent()
+        err_msg = handled_exc.get_message_for_exception()
+        sly.logger.error(err_msg, extra=debug_info, exc_info=True)
     else:
         err_msg = repr(exc)
         if len(err_msg) > 255:
             err_msg = err_msg[:252] + "..."
         g.api.task.set_output_error(g.task_id, msg, err_msg)
-        sly.logger.error(f"{msg}. {repr(exc)}")
-    sly.logger.info(
-        f"Debug info:\n"
-        f"    team_id={g.team_id}\n    workspace_id={g.workspace_id}\n"
-        f"    project_id={g.project_id}\n    dataset_id={g.dataset_id}\n"
-        f"    project_modality={g.project_modality}\n    src_dir={g.input_path}\n"
-    )
+        sly.logger.error(f"{msg}. {repr(exc)}", extra=debug_info, exc_info=True)
     sly.fs.clean_dir(g.app_data)
     exit(0)
