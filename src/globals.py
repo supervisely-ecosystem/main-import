e0bf3a09f03d75e87105d1e06988dd29e3668120
almaz <almazgimaev@gmail.com>
2024-05-08 11:05:54 +0200

    check dataset name

--- src/globals.py
+++ src/globals.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 from dotenv import load_dotenv
@@ -18,7 +19,14 @@ team_id = sly.env.team_id()
 workspace_id = sly.env.workspace_id()
 project_id = sly.env.project_id()
 dataset_id = sly.env.dataset_id(raise_not_found=False)
-dataset_name = os.environ.get("modal.state.datasetName", "ds0")
+
+default_ds_name = f"dataset {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+dataset_name = os.environ.get("modal.state.datasetName", default_ds_name)
+if dataset_name == "":
+    dataset_name = default_ds_name
+existing_ds_names = set([ds.name for ds in api.dataset.get_list(project_id, recursive=True)])
+dataset_name = sly.generate_free_name(existing_ds_names, dataset_name, False, True)
+
 input_path = sly.env.folder(raise_not_found=False)
 if input_path is None:
     input_path = sly.env.file(raise_not_found=False)
