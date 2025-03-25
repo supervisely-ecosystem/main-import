0d45083a3d5b5b44e872d325e012530bb725f1cf
almaz <almazgimaev@gmail.com>
2025-03-25 11:24:55 +0100

    wip

--- src/tests/test_import.py
+++ src/tests/test_import.py
@@ -7,7 +7,19 @@ from supervisely.project.project_settings import LabelingInterface
 
 if sly.is_development():
     load_dotenv("local.env")
-    load_dotenv(os.path.expanduser("~/supervisely.env"))
+    load_dotenv(os.path.expanduser("~/demo_supervisely.env"))
+
+sly.json.load_json_file
+class bcolors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
 
 
 api = sly.Api()
@@ -15,21 +27,25 @@ api = sly.Api()
 team_id = sly.env.team_id()  # test data in team ID:447
 workspace_id = sly.env.workspace_id()
 
-agent_id = 452
-module_id = 707
-module_info = api.app.get_ecosystem_module_info(module_id)
+# agent_id = 452
+# module_id = 707
+# module_info = api.app.get_ecosystem_module_info(module_id)
 
 test_dirs_paths = {
     # "local": "/Users/almaz/job/supervisely/projects/main-import/test_files",
-    "team_files": "/TESTS/",
-    "s3": "s3://remote-img-test/TESTS-NEW-IMPORT/",
-    "azure": "azure://supervisely-test/TEST-NEW-IMPORT/",
-    "google": "google://sly-dev-test/TEST-NEW-IMPORT/",
+    "team_files": "/test_autoimport/",
+    # "s3": "s3://remote-img-test/TESTS-NEW-IMPORT/",
+    # "azure": "azure://supervisely-test/TEST-NEW-IMPORT/",
+    # "google": "google://sly-dev-test/TEST-NEW-IMPORT/",
 }
 
+res_success, res_failed = [], []
 
+import logging
+sly.logger.setLevel(logging.ERROR)
 # # UPLOAD ALL TEST FOLDERS
 for source, test_dir in test_dirs_paths.items():
+    print(f"{bcolors.HEADER}Source: {source}{bcolors.ENDC}")
     if sly.fs.dir_exists(test_dir):
         test_dirs = [
             os.path.join(test_dir, d)
@@ -47,6 +63,7 @@ for source, test_dir in test_dirs_paths.items():
         exit(0)
 
     for project_type in sly.ProjectType:
+        print(f"{bcolors.OKCYAN}Project type: {project_type}{bcolors.ENDC}")
         project_name = f"{source}_{str(project_type)}"
         project = api.project.create(
             workspace_id,
@@ -54,118 +71,148 @@ for source, test_dir in test_dirs_paths.items():
             change_name_if_conflict=True,
             type=project_type,
         )
-        if project_type == sly.ProjectType.IMAGES:
-            multi_view_project = api.project.create(
-                workspace_id,
-                project_name + "_multi_view",
-                change_name_if_conflict=True,
-                type=project_type,
-            )
-            multispectral_project = api.project.create(
-                workspace_id,
-                project_name + "_multispectral",
-                change_name_if_conflict=True,
-                type=project_type,
-            )
-            medical2d_project = api.project.create(
-                workspace_id,
-                project_name + "_medical2d",
-                change_name_if_conflict=True,
-                type=project_type,
-            )
-            matting_project = api.project.create(
-                workspace_id,
-                project_name + "_matting",
-                change_name_if_conflict=True,
-                type=project_type,
-            )
+        # if project_type == sly.ProjectType.IMAGES:
+        #     multi_view_project = api.project.create(
+        #         workspace_id,
+        #         project_name + "_multi_view",
+        #         change_name_if_conflict=True,
+        #         type=project_type,
+        #     )
+        #     multispectral_project = api.project.create(
+        #         workspace_id,
+        #         project_name + "_multispectral",
+        #         change_name_if_conflict=True,
+        #         type=project_type,
+        #     )
+        #     medical2d_project = api.project.create(
+        #         workspace_id,
+        #         project_name + "_medical2d",
+        #         change_name_if_conflict=True,
+        #         type=project_type,
+        #     )
+        #     matting_project = api.project.create(
+        #         workspace_id,
+        #         project_name + "_matting",
+        #         change_name_if_conflict=True,
+        #         type=project_type,
+        #     )
+        upload_as_links = False
+        # if source in ["local", "team_files"] and upload_as_links:
+        #     continue  # upload as links applicable only for cloud storages
+        if sly.fs.dir_exists("/Users/almaz/job/supervisely/projects/main-import/temp"):
+            sly.fs.clean_dir("/Users/almaz/job/supervisely/projects/main-import/temp")
+        # sly.logger.info(f"Upload as links: {upload_as_links}")
+        print(f"{bcolors.OKBLUE}Upload as links: {upload_as_links}{bcolors.ENDC}")
         for path in test_dirs:
-            if str(project_type) in path:
+            dir_name = os.path.basename(os.path.normpath(path))
+            if str(project_type) in dir_name:
                 labeling_interface = LabelingInterface.DEFAULT
                 curr_project = project
                 if "multi-view" in path:
+                    continue
                     labeling_interface = LabelingInterface.MULTIVIEW
                     curr_project = multi_view_project
                 elif "multispectral" in path:
+                    continue
                     labeling_interface = LabelingInterface.MULTISPECTRAL
                     curr_project = multispectral_project
                 elif "medical2d" in path:
+                    continue
                     labeling_interface = LabelingInterface.MEDICAL_IMAGING_SINGLE
                     curr_project = medical2d_project
                 elif "matting" in path:
+                    continue
                     labeling_interface = LabelingInterface.IMAGE_MATTING
                     curr_project = matting_project
 
                 if "links" in path:
+                    continue
                     upload_as_links = True
 
                 dataset_name = os.path.basename(path.rstrip("/"))
                 dataset = api.dataset.create(
                     curr_project.id, dataset_name, change_name_if_conflict=True
                 )
-                importer = sly.ImportManager(
-                    path, project_type, labeling_interface=labeling_interface
-                )
-                importer.upload_dataset(dataset.id)
+                try:
 
-        remote_path = None
-        if project_type == sly.ProjectType.IMAGES:
-            remote_path = "s3://remote-img-test/TESTS-NEW-IMPORT/01. images SLY (from export)/"
-        elif project_type == sly.ProjectType.VIDEOS:
-            remote_path = "s3://remote-img-test/TESTS-NEW-IMPORT/09. videos SLY (from export)/"
-        if remote_path is not None:
-            dataset_name = os.path.basename(remote_path.rstrip("/"))
-            dataset = api.dataset.create(project.id, dataset_name, change_name_if_conflict=True)
-            importer = sly.ImportManager(remote_path, project_type, upload_as_links=True)
-            importer.upload_dataset(dataset.id)
+                    importer = sly.ImportManager(
+                        path,
+                        project_type,
+                        labeling_interface=labeling_interface,
+                        upload_as_links=upload_as_links,
+                    )
+                    importer.upload_dataset(dataset.id)
 
-                # if project_type == sly.ProjectType.IMAGES:
-                #     params = module_info.get_arguments(
-                #         files_folder=path, images_project=project.id, images_dataset=dataset.id
-                #     )
-                # elif project_type == sly.ProjectType.VIDEOS:
-                #     params = module_info.get_arguments(
-                #         files_folder=path, videos_project=project.id, videos_dataset=dataset.id
-                #     )
-                # elif project_type == sly.ProjectType.POINT_CLOUDS:
-                #     params = module_info.get_arguments(
-                #         files_folder=path,
-                #         point_cloud_project=project.id,
-                #         point_cloud_dataset=dataset.id,
-                #     )
-                # elif project_type == sly.ProjectType.VOLUMES:
-                #     params = module_info.get_arguments(
-                #         files_folder=path, volumes_project=project.id, volumes_dataset=dataset.id
-                #     )
-                # elif project_type == sly.ProjectType.POINT_CLOUD_EPISODES:
-                #     params = module_info.get_arguments(
-                #         files_folder=path,
-                #         point_cloud_episodes_project=project.id,
-                #         point_cloud_episodes_dataset=dataset.id,
-                #     )
-                # else:
-                #     continue
-                # session = api.app.start(
-                #     agent_id=agent_id,
-                #     module_id=module_id,
-                #     workspace_id=workspace_id,
-                #     task_name=f"{source}",
-                #     params=params,
-                # )
-                # try:
-                #     api.app.wait(
-                #         session.task_id,
-                #         target_status=api.task.Status.FINISHED,
-                #         attempts=25,
-                #         attempt_delay_sec=5,
-                #     )
+                    print(f"{bcolors.OKGREEN}Uploaded: {path}{bcolors.ENDC}")
+                    res_success.append(path)
+                except Exception as e:
+                    print(f"{bcolors.FAIL}Failed: {path}{bcolors.ENDC}")
+                    print(e)
+                    res_failed.append(path)
 
-                # except sly.WaitingTimeExceeded as e:
-                #     print(e)
-                #     api.app.stop(session.task_id)
-                # except sly.TaskFinishedWithError as e:
-                #     print(e)
-                # print("Task status: ", api.app.get_status(session.task_id))
+print(f"{bcolors.OKCYAN}==============================={bcolors.ENDC}")
+print(f"{bcolors.HEADER} Finished!{bcolors.ENDC}")
+print(f"{bcolors.OKGREEN}Success: {len(res_success)}{bcolors.ENDC}")
+print(f"{bcolors.FAIL}Failed: {len(res_failed)}{bcolors.ENDC}")
+print(f"{bcolors.OKCYAN}==============================={bcolors.ENDC}")
+# remote_path = None
+# if project_type == sly.ProjectType.IMAGES:
+#     remote_path = "s3://remote-img-test/TESTS-NEW-IMPORT/01. images SLY (from export)/"
+# elif project_type == sly.ProjectType.VIDEOS:
+#     remote_path = "s3://remote-img-test/TESTS-NEW-IMPORT/09. videos SLY (from export)/"
+# if remote_path is not None:
+#     dataset_name = os.path.basename(remote_path.rstrip("/"))
+#     dataset = api.dataset.create(project.id, dataset_name, change_name_if_conflict=True)
+#     importer = sly.ImportManager(remote_path, project_type, upload_as_links=True)
+#     importer.upload_dataset(dataset.id)
+
+# if project_type == sly.ProjectType.IMAGES:
+#     params = module_info.get_arguments(
+#         files_folder=path, images_project=project.id, images_dataset=dataset.id
+#     )
+# elif project_type == sly.ProjectType.VIDEOS:
+#     params = module_info.get_arguments(
+#         files_folder=path, videos_project=project.id, videos_dataset=dataset.id
+#     )
+# elif project_type == sly.ProjectType.POINT_CLOUDS:
+#     params = module_info.get_arguments(
+#         files_folder=path,
+#         point_cloud_project=project.id,
+#         point_cloud_dataset=dataset.id,
+#     )
+# elif project_type == sly.ProjectType.VOLUMES:
+#     params = module_info.get_arguments(
+#         files_folder=path, volumes_project=project.id, volumes_dataset=dataset.id
+#     )
+# elif project_type == sly.ProjectType.POINT_CLOUD_EPISODES:
+#     params = module_info.get_arguments(
+#         files_folder=path,
+#         point_cloud_episodes_project=project.id,
+#         point_cloud_episodes_dataset=dataset.id,
+#     )
+# else:
+#     continue
+# session = api.app.start(
+#     agent_id=agent_id,
+#     module_id=module_id,
+#     workspace_id=workspace_id,
+#     task_name=f"{source}",
+#     params=params,
+# )
+# try:
+#     api.app.wait(
+#         session.task_id,
+#         target_status=api.task.Status.FINISHED,
+#         attempts=25,
+#         attempt_delay_sec=5,
+#     )
+
+# except sly.WaitingTimeExceeded as e:
+#     print(e)
+#     api.app.stop(session.task_id)
+# except sly.TaskFinishedWithError as e:
+#     print(e)
+# print("Task status: ", api.app.get_status(session.task_id))
 
 # # UPLOAD SINGLE FOLDER
 # project_id = sly.env.project_id(raise_not_found=False)
